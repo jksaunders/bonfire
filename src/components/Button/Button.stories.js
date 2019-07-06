8fde0ac7de67783fed58c3972b7e49f25f5a446e
Joshua Saunders <jk.saunders@hotmail.com>
2019-07-06 15:54:55 -0400

    Added Istanbul - fine-tuned eslint rules - added unit test

--- src/components/Button/Button.stories.js
+++ src/components/Button/Button.stories.js
@@ -1,15 +1,15 @@
 /* eslint-disable import/no-extraneous-dependencies */
-import React from 'react';
+import React from "react";
 
-import { storiesOf } from '@storybook/react';
-import { action } from '@storybook/addon-actions';
+import { storiesOf } from "@storybook/react";
+import { action } from "@storybook/addon-actions";
 
-import Button from './Button';
+import Button from "./Button";
 
-storiesOf('Button', module)
-  .add('with text', () => <Button onClick={action('clicked')}>Hello Button</Button>)
-  .add('with some emoji', () => (
-    <Button onClick={action('clicked')}>
+storiesOf("Button", module)
+  .add("with text", () => <Button onClick={action("clicked")}>Hello Button</Button>)
+  .add("with some emoji", () => (
+    <Button onClick={action("clicked")}>
       <span role="img" aria-label="so cool">
         😀 😎 👍 💯
       </span>
---
+++ test/index.spec.js
@@ -0,0 +1,7 @@
+import { assert } from "chai";
+
+describe("Math!", () => {
+  it("should work as expected", () => {
+    assert((0 + 1) === 1, "How??");
+  });
+});
