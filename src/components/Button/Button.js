56a8976bc5ca47f06b1e324cef794bc71981cecf
Joshua Saunders <jk.saunders@hotmail.com>
2019-07-08 15:33:36 -0400

    Added styled-components, addon-knobs and used ThemeProvider

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,7 +1,31 @@
 import React from "react";
+import PropTypes from "prop-types";
+import styled from "styled-components";
+import theme from "styled-theming";
 
-const Button = () => (
-  <button />
+const backgroundColor = theme("mode", {
+  light: "#fafafa",
+  dark: "#222"
+});
+
+const StyledButton = styled.button`
+  background-color: ${backgroundColor}
+`;
+
+const propTypes = {
+  children: PropTypes.node
+};
+
+const defaultProps = {
+  children: null
+};
+
+
+const Button = ({ children }) => (
+  <StyledButton>{children}</StyledButton>
 );
+
+Button.propTypes = propTypes;
+Button.defaultProps = defaultProps;
 
 export default Button;
--- src/components/Button/Button.stories.js
+++ src/components/Button/Button.stories.js
@@ -1,17 +1,27 @@
-/* eslint-disable import/no-extraneous-dependencies */
 import React from "react";
+import { ThemeProvider } from "styled-components";
 
 import { storiesOf } from "@storybook/react";
 import { action } from "@storybook/addon-actions";
+import { select, withKnobs } from "@storybook/addon-knobs";
 
 import Button from "./Button";
 
 storiesOf("Button", module)
+  .addDecorator(withKnobs)
   .add("with text", () => <Button onClick={action("clicked")}>Hello Button</Button>)
-  .add("with some emoji", () => (
-    <Button onClick={action("clicked")}>
-      <span role="img" aria-label="so cool">
-        😀 😎 👍 💯
-      </span>
-    </Button>
-  ));
+  .add("with some emoji", () => {
+    const themeMode = select("Theme Mode", ["light", "dark"], "light");
+    const theme = { mode: themeMode };
+    return (
+      <ThemeProvider theme={theme}>
+        <Button onClick={action("clicked")}>
+          <span role="img" aria-label="so cool">
+            😀 😎 👍 💯
+            {" "}
+            {themeMode}
+          </span>
+        </Button>
+      </ThemeProvider>
+    );
+  });
