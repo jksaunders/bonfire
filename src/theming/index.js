f3a893a1f960fb8fe825de89373ec92b3943c822
Joshua Saunders <jk.saunders@hotmail.com>
2019-09-21 14:33:17 -0400

    Export colors

--- src/theming/index.js
+++ src/theming/index.js
@@ -1,5 +1,7 @@
 import * as importedPalette from "./palette";
+import * as importedColors from "./colors";
 
 export { theme, themeVariant } from "./theme";
 export { ThemeConstants } from "./themes/constants";
+export const colors = importedColors;
 export const Palette = importedPalette;
