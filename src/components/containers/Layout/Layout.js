0887b75186db269672e757271b4be0a67356df1c
Joshua Saunders <jk.saunders@hotmail.com>
2019-11-25 19:53:54 -0500

    Updated Layout to use CSS grid

--- src/components/containers/Layout/Layout.js
+++ src/components/containers/Layout/Layout.js
@@ -1,194 +1,77 @@
 import React from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
-import { isArray } from "util";
-
-const CONSTANTS = {
-  JUSTIFY_CONTENT_ALIGN_ITEMS_KEYS: [
-    "center",
-    "start",
-    "end",
-    "flex-start",
-    "flex-end",
-    "left",
-    "right",
-    "normal",
-    "space-between",
-    "space-around",
-    "space-evenly",
-    "stretch"
-  ],
-  ORIENTATION: {
-    HORIZONTAL: "horizontal",
-    VERTICAL: "vertical"
-  },
-  SPACING: {
-    SMALL: "small",
-    MEDIUM: "medium",
-    LARGE: "large"
-  }
-};
-
-const SPACING_VALUES = {
-  [CONSTANTS.SPACING.SMALL]: "4px",
-  [CONSTANTS.SPACING.MEDIUM]: "8px",
-  [CONSTANTS.SPACING.LARGE]: "16px",
-};
 
 const propTypes = {
   children: PropTypes.node,
-  childrenFlex: PropTypes.oneOfType([
-    PropTypes.number,
-    PropTypes.arrayOf(PropTypes.number),
-    PropTypes.func
-  ]),
-  childrenMargin: PropTypes.oneOfType([
-    PropTypes.string,
-    PropTypes.arrayOf(PropTypes.string),
-    PropTypes.func
-  ]),
-  flexWrapReverse: PropTypes.bool,
-  fullHeightWidth: PropTypes.bool,
-  horizontalAlignment: PropTypes.oneOf(CONSTANTS.JUSTIFY_CONTENT_ALIGN_ITEMS_KEYS),
-  innerContentWidth: PropTypes.string,
-  linesAlignment: PropTypes.oneOf(CONSTANTS.JUSTIFY_CONTENT_ALIGN_ITEMS_KEYS),
-  orientation: PropTypes.oneOf(Object.values(CONSTANTS.ORIENTATION)),
-  padding: PropTypes.oneOf(Object.values(CONSTANTS.SPACING)),
-  verticalAlignment: PropTypes.oneOf(CONSTANTS.JUSTIFY_CONTENT_ALIGN_ITEMS_KEYS)
+  columns: PropTypes.string,
+  gap: PropTypes.string,
+  height: PropTypes.string,
+  horizontalAlignment: PropTypes.string,
+  rows: PropTypes.string,
+  padding: PropTypes.string,
+  verticalAlignment: PropTypes.string,
+  width: PropTypes.string
 };
 
 const defaultProps = {
   children: null,
-  childrenFlex: 1,
-  childrenMargin: null,
-  flexWrapReverse: false,
-  fullHeightWidth: false,
-  horizontalAlignment: "normal",
-  innerContentWidth: null,
-  linesAlignment: null,
-  orientation: CONSTANTS.ORIENTATION.HORIZONTAL,
+  columns: null,
+  gap: null,
+  height: "100%",
+  horizontalAlignment: null,
+  rows: null,
   padding: null,
-  verticalAlignment: "normal"
+  verticalAlignment: null,
+  width: "100%"
 };
 
-const getFlexDirection = ({ orientation }) => {
-  let direction = "";
-  switch (orientation) {
-    case CONSTANTS.ORIENTATION.HORIZONTAL:
-      direction = "row";
-      break;
-    case CONSTANTS.ORIENTATION.VERTICAL:
-      direction = "column";
-      break;
-    default: return null;
+const css = (prop, key) => (props) => {
+  if (props[prop] == null) {
+    return null;
   }
-  return `flex-direction: ${direction};`;
+  const cssKey = key || prop;
+  return `${cssKey}: ${props[prop]};`;
 };
 
-const getJustifyContentAndAlignItems = ({
-  horizontalAlignment,
-  orientation,
-  verticalAlignment
-}) => `
-  align-items: ${orientation === CONSTANTS.ORIENTATION.HORIZONTAL ? verticalAlignment : horizontalAlignment};
-  justify-content: ${orientation === CONSTANTS.ORIENTATION.HORIZONTAL ? horizontalAlignment : verticalAlignment};
+const Grid = styled.div`
+  display: grid;
+  ${css("rows", "grid-template-rows")}
+  ${css("columns", "grid-template-columns")}
+  ${css("gap", "grid-gap")}
+  ${css("height")}
+  ${css("horizontalAlignment", "justify-items")}
+  ${css("padding")}
+  ${css("verticalAlignment", "align-items")}
+  ${css("width")}
 `;
-
-const getDescendentValues = ({ children, childrenFlex, childrenMargin }) => {
-  if (children == null || children.length === 0) {
-    return "";
-  }
-
-  const allChildrenValues = {};
-  const childrenValues = [...children.map(() => ({}))];
-
-  if (typeof childrenFlex === "number") {
-    allChildrenValues.flex = childrenFlex;
-  } else if (typeof childrenFlex === "function") {
-    children.forEach((e, index) => {
-      childrenValues[index].flex = childrenFlex(index);
-    });
-  } else if (isArray(childrenFlex)) {
-    children.forEach((e, index) => {
-      childrenValues[index].flex = childrenFlex[index];
-    });
-  }
-
-  if (typeof childrenMargin === "string") {
-    allChildrenValues.margin = childrenMargin;
-  } else if (typeof childrenMargin === "function") {
-    children.forEach((e, index) => {
-      childrenValues[index].margin = childrenMargin(index);
-    });
-  } else if (isArray(childrenMargin)) {
-    children.forEach((e, index) => {
-      childrenValues[index].margin = childrenMargin[index];
-    });
-  }
-
-  let result = "";
-  const getSpacingValueForMargin = margin => (
-    Object.values(CONSTANTS.SPACING).indexOf(margin) > -1 ? SPACING_VALUES[margin] : margin
-  );
-
-  if (Object.keys(allChildrenValues).length > 0) {
-    result += `
-      && > * {
-        ${allChildrenValues.flex != null ? `flex: ${allChildrenValues.flex}` : ""}
-        ${allChildrenValues.margin != null ? `margin: ${getSpacingValueForMargin(allChildrenValues.margin)}` : ""}
-      }
-    `;
-  }
-
-  childrenValues.forEach((values, index) => {
-    if (Object.keys(values).length > 0) {
-      result += `
-        && > :nth-child(${index + 1}) {
-          ${values.flex != null ? `flex: ${values.flex}` : ""}
-          ${values.margin != null ? `margin: ${getSpacingValueForMargin(values.margin)}` : ""}
-        }
-      `;
-    }
-  });
-
-  return result;
-};
-
-const getPadding = ({ padding }) => (padding != null ? `padding: ${SPACING_VALUES[padding]};` : "");
-
-const getAlignContent = ({ linesAlignment }) => (linesAlignment != null ? `align-content: ${linesAlignment};` : "");
-
-const Flex = styled.div`
-    box-sizing: border-box;
-    display: flex;
-    flex-wrap: ${props => (props.flexWrapReverse ? "wrap-reverse" : "wrap")};
-    height: ${props => (props.fullHeightWidth ? "100%" : "auto")};
-    width: ${props => (props.fullHeightWidth ? "100%" : "auto")};
-
-    ${getAlignContent}
-    ${getDescendentValues}
-    ${getFlexDirection}
-    ${getJustifyContentAndAlignItems}
-    ${getPadding}
-`;
-
-export const InnerContentWidthContext = React.createContext(null);
 
 const Layout = ({
   children,
-  childrenFlex,
-  innerContentWidth,
-  ...rest
+  columns,
+  gap,
+  height,
+  horizontalAlignment,
+  rows,
+  padding,
+  verticalAlignment,
+  width,
 }) => (
-  <InnerContentWidthContext.Provider value={innerContentWidth}>
-    <Flex childrenFlex={childrenFlex} {...rest}>
-      {children}
-    </Flex>
-  </InnerContentWidthContext.Provider>
+  <Grid
+    columns={columns}
+    gap={gap}
+    height={height}
+    horizontalAlignment={horizontalAlignment}
+    rows={rows}
+    padding={padding}
+    verticalAlignment={verticalAlignment}
+    width={width}
+  >
+    {children}
+  </Grid>
 );
 
 Layout.propTypes = propTypes;
 Layout.defaultProps = defaultProps;
-Layout.CONSTANTS = CONSTANTS;
 
 export default Layout;
--- src/components/containers/Layout/Layout.stories.js
+++ src/components/containers/Layout/Layout.stories.js
@@ -3,8 +3,6 @@ import styled from "styled-components";
 
 import { storiesOf } from "@storybook/react";
 import {
-  boolean,
-  select,
   text,
   withKnobs
 } from "@storybook/addon-knobs";
@@ -26,52 +24,26 @@ storiesOf("Layout", module)
   .addDecorator(withKnobs)
   .add("basic", () => {
     const containerSize = text("Size of container", "500px");
-    const flexWrapReverse = boolean("Reverse flex-wrap?", false);
-    const orientation = select("Orientation", Object.values(Layout.CONSTANTS.ORIENTATION), Object.values(Layout.CONSTANTS.ORIENTATION)[0]);
-    const horizontalAlignment = select("Horizontal Alignment", Layout.CONSTANTS.JUSTIFY_CONTENT_ALIGN_ITEMS_KEYS, "normal");
-    const verticalAlignment = select("Vertical Alignment", Layout.CONSTANTS.JUSTIFY_CONTENT_ALIGN_ITEMS_KEYS, "normal");
+    const columns = text("Columns", "");
+    const gap = text("Gap", "");
+    const horizontalAlignment = text("Horizontal alignment", "");
+    const rows = text("Rows", "");
+    const padding = text("Padding", "");
+    const verticalAlignment = text("Vertical alignment", "");
 
     return (
       <StoryWrapper size={containerSize}>
         <Layout
-          flexWrapReverse={flexWrapReverse}
-          horizontalAlignment={horizontalAlignment}
-          orientation={orientation}
-          verticalAlignment={verticalAlignment}
+          rows={rows !== "" ? rows : null}
+          columns={columns !== "" ? columns : null}
+          gap={gap !== "" ? gap : null}
+          horizontalAlignment={horizontalAlignment !== "" ? horizontalAlignment : null}
+          padding={padding !== "" ? padding : null}
+          verticalAlignment={verticalAlignment !== "" ? verticalAlignment : null}
         >
           <Content><Typography.H2>content1</Typography.H2></Content>
           <Content><Typography.H2>content2</Typography.H2></Content>
           <Content><Typography.H2>content3</Typography.H2></Content>
-        </Layout>
-      </StoryWrapper>
-    );
-  }).add("nested", () => {
-    const containerSize = text("Size of container", "500px");
-    const flexWrapReverse = boolean("Reverse flex-wrap?", false);
-    const varyingFlexValues = boolean("Varying flex values?", false);
-    const orientation = select("Orientation", Object.values(Layout.CONSTANTS.ORIENTATION), Object.values(Layout.CONSTANTS.ORIENTATION)[0]);
-    const horizontalAlignment = select("Horizontal Alignment", Layout.CONSTANTS.JUSTIFY_CONTENT_ALIGN_ITEMS_KEYS, "normal");
-    const verticalAlignment = select("Vertical Alignment", Layout.CONSTANTS.JUSTIFY_CONTENT_ALIGN_ITEMS_KEYS, "normal");
-    const linesAlignment = select("Lines Alignment", Layout.CONSTANTS.JUSTIFY_CONTENT_ALIGN_ITEMS_KEYS, "stretch");
-
-    return (
-      <StoryWrapper size={containerSize}>
-        <Layout
-          childrenFlex={varyingFlexValues ? i => (i === 0 ? 0 : 1) : 1}
-          childrenMargin={Layout.CONSTANTS.SPACING.LARGE}
-          flexWrapReverse={flexWrapReverse}
-          fullHeightWidth
-          horizontalAlignment={horizontalAlignment}
-          linesAlignment={linesAlignment}
-          orientation={orientation}
-          padding={Layout.CONSTANTS.SPACING.LARGE}
-          verticalAlignment={verticalAlignment}
-        >
-          <Content><Typography.H2>content1</Typography.H2></Content>
-          <Layout orientation={Layout.CONSTANTS.ORIENTATION.VERTICAL}>
-            <Content><Typography.H2>content2</Typography.H2></Content>
-            <Content><Typography.H2>content3</Typography.H2></Content>
-          </Layout>
         </Layout>
       </StoryWrapper>
     );
