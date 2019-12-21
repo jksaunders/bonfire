a174a47904d7e9fbc7565199c0d9dfc90457963c
Joshua Saunders <jk.saunders@hotmail.com>
2019-12-21 16:21:45 -0500

    Removed palette and sizing

--- src/theming/index.js
+++ src/theming/index.js
@@ -1,5 +1,3 @@
-import * as importedPalette from './palette';
 import * as importedColors from './colors';
 
 export const colors = importedColors;
-export const Palette = importedPalette;
--- src/theming/sizing.js
+++
@@ -1,3 +0,0 @@
-export const s = '4px';
-export const m = '8px';
-export const l = '16px';
