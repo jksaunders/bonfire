5a92b842e4cd821415089aced046114a9fbc217c
Joshua Saunders <jk.saunders@hotmail.com>
2019-12-23 02:21:21 -0500

    Moved from webpack to rollup

--- babel.config.js
+++ babel.config.js
@@ -1,15 +1,33 @@
-module.exports = (api) => {
+module.exports = api => {
   api.cache.using(() => process.env.NODE_ENV);
 
   let presets = [];
 
   if (api.env('production')) {
+    presets = [
+      [
+        '@babel/preset-env',
+        {
+          modules: false,
+        },
+      ],
+      '@babel/preset-react',
+    ];
+  } else if (api.env('test')) {
     presets = ['@babel/preset-env', '@babel/preset-react'];
   } else {
-    presets = ['@babel/preset-env', '@babel/preset-react'];
+    presets = [
+      [
+        '@babel/preset-env',
+        {
+          modules: false,
+        },
+      ],
+      '@babel/preset-react',
+    ];
   }
 
   return {
-    presets
+    presets,
   };
 };
---
+++ rollup.config.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+import externals from 'rollup-plugin-node-externals';
+import babel from 'rollup-plugin-babel';
+import localResolve from 'rollup-plugin-local-resolve';
+
+import pkg from './package.json';
+
+const plugins = [
+  externals(),
+  localResolve(),
+  resolve(),
+  babel({
+    exclude: 'node_modules/**',
+  }),
+  commonjs({
+    namedExports: {
+      react: Object.keys(React),
+      'react-dom': Object.keys(ReactDOM),
+      'react-is': ['isElement', 'isValidElementType', 'ForwardRef'],
+    },
+  }),
+];
+
+export default [
+  // browser-friendly UMD build
+  {
+    input: 'src/index.js',
+    output: {
+      name: 'bonfire',
+      file: pkg.browser,
+      format: 'umd',
+    },
+    plugins: [...plugins],
+  },
+
+  // CommonJS (for Node) and ES module (for bundlers) build.
+  // (We could have three entries in the configuration array
+  // instead of two, but it's quicker to generate multiple
+  // builds from a single configuration where possible, using
+  // an array for the `output` option, where we can specify
+  // `file` and `format` for each target)
+  {
+    input: 'src/index.js',
+    output: [
+      { file: pkg.main, format: 'cjs' },
+      { file: pkg.module, format: 'es' },
+    ],
+    plugins: [...plugins],
+  },
+];
