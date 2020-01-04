43bfa060fa01d7e2ff1c083d628e38aadf12c55c
Joshua Saunders <jk.saunders@hotmail.com>
2020-01-04 17:32:15 -0500

    Box exported correctly

--- src/components/containers/index.js
+++ src/components/containers/index.js
@@ -1,4 +1,5 @@
 export { default as Bar } from './Bar';
+export { default as Box } from './Box';
 export { default as Dialog } from './Dialog';
 export { default as Drawer } from './Drawer';
 export { default as Layout } from './Layout';
