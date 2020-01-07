273a82aff0e623d5804633ab3e28d4f045e9e061
Joshua Saunders <jk.saunders@hotmail.com>
2020-01-07 12:40:28 -0500

    Css reset moved to be its own component

--- src/components/containers/Box/Box.js
+++ src/components/containers/Box/Box.js
@@ -41,15 +41,6 @@ export const BoxDefaultProps = fromEntries(
   Object.entries(BoxPropTypes).map(e => [e[0], null])
 );
 
-const cssReset = `
-  margin: 0;
-  padding: 0;
-  border: 0;
-  font-size: 100%;
-  font: inherit;
-  vertical-align: baseline;
-`;
-
 const materialPaperElevations = [
   'none',
   '0px 2px 1px -1px rgba(0,0,0,0.2), 0px 1px 1px 0px rgba(0,0,0,0.14), 0px 1px 3px 0px rgba(0,0,0,0.12)',
@@ -79,9 +70,6 @@ const materialPaperElevations = [
 ];
 
 const StyledBox = styled.div`
-  ${/* reset */ ''}
-  ${cssReset}
-  
   box-sizing: border-box;
 
   ${/* design */ ''}
--- src/styling/index.js
+++ src/styling/index.js
@@ -1,4 +1,5 @@
 export { default as cssBackground } from './cssBackground';
 export { default as cssBorder } from './cssBorder';
+export { default as CssReset } from './CssReset';
 export * from './css';
 export * from './Responsive';
