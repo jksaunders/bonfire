728110f8bbc8432cc6ed5a7b000b7d530ef101f2
Joshua Saunders <jk.saunders@hotmail.com>
2020-12-23 18:28:49 -0500

    Tests for responsive props, no responsive usage

--- src/utils/CssRule/CssRule.spec.ts
+++ src/utils/CssRule/CssRule.spec.ts
@@ -1,5 +1,5 @@
 import { CssRule, processCssRule } from './CssRule';
-// import { ResponsiveProps } from '../ResponsiveProps';
+import { ResponsiveProps } from '../ResponsiveProps';
 
 interface ExampleProps {
   color?: string;
@@ -14,93 +14,93 @@ interface TestCase<T> {
   title: string;
 }
 
-const testCases: TestCase<ExampleProps>[] = [
-  {
-    args: ['color'],
-    expected: '',
-    props: {
-      height: 'xs',
-      width: 'full',
-    },
-    title: 'missing prop',
-  },
-  {
-    args: ['color'],
-    expected: 'color: red;',
-    props: {
-      color: 'red',
-      height: 'xs',
-      width: 'full',
-    },
-    title: 'prop present',
-  },
-  {
-    args: ['height', 'height-key'],
-    expected: 'height-key: xs;',
-    props: {
-      color: 'red',
-      height: 'xs',
-      width: 'full',
-    },
-    title: 'string key',
-  },
-  {
-    args: ['height', undefined, 'tall'],
-    expected: 'height: tall;',
-    props: {
-      color: 'red',
-      height: 'xs',
-      width: 'full',
-    },
-    title: 'string value, no key',
-  },
-  {
-    args: ['height', 'height-key', 'tall'],
-    expected: 'height-key: tall;',
-    props: {
-      color: 'red',
-      height: 'xs',
-      width: 'full',
-    },
-    title: 'string key and value',
-  },
-  {
-    args: ['height', (props): string => props.width],
-    expected: 'full: xs;',
-    props: {
-      color: 'red',
-      height: 'xs',
-      width: 'full',
-    },
-    title: 'function key',
-  },
-  {
-    args: ['height', (props): string => props.width, 'purple'],
-    expected: 'full: purple;',
-    props: {
-      color: 'red',
-      height: 'xs',
-      width: 'full',
-    },
-    title: 'function key, string value',
-  },
-  {
-    args: [
-      'height',
-      (props): string => props.width,
-      (props): string => props.height + ' x2',
-    ],
-    expected: 'full: xs x2;',
-    props: {
-      color: 'red',
-      height: 'xs',
-      width: 'full',
-    },
-    title: 'function key and value',
-  },
-];
-
 describe('processCssRule: regular props', () => {
+  const testCases: TestCase<ExampleProps>[] = [
+    {
+      args: ['color'],
+      expected: '',
+      props: {
+        height: 'xs',
+        width: 'full',
+      },
+      title: 'missing prop',
+    },
+    {
+      args: ['color'],
+      expected: 'color: red;',
+      props: {
+        color: 'red',
+        height: 'xs',
+        width: 'full',
+      },
+      title: 'prop present',
+    },
+    {
+      args: ['height', 'height-key'],
+      expected: 'height-key: xs;',
+      props: {
+        color: 'red',
+        height: 'xs',
+        width: 'full',
+      },
+      title: 'string key',
+    },
+    {
+      args: ['height', undefined, 'tall'],
+      expected: 'height: tall;',
+      props: {
+        color: 'red',
+        height: 'xs',
+        width: 'full',
+      },
+      title: 'string value, no key',
+    },
+    {
+      args: ['height', 'height-key', 'tall'],
+      expected: 'height-key: tall;',
+      props: {
+        color: 'red',
+        height: 'xs',
+        width: 'full',
+      },
+      title: 'string key and value',
+    },
+    {
+      args: ['height', (props): string => props.width],
+      expected: 'full: xs;',
+      props: {
+        color: 'red',
+        height: 'xs',
+        width: 'full',
+      },
+      title: 'function key',
+    },
+    {
+      args: ['height', (props): string => props.width, 'purple'],
+      expected: 'full: purple;',
+      props: {
+        color: 'red',
+        height: 'xs',
+        width: 'full',
+      },
+      title: 'function key, string value',
+    },
+    {
+      args: [
+        'height',
+        (props): string => props.width,
+        (props): string => props.height + ' x2',
+      ],
+      expected: 'full: xs x2;',
+      props: {
+        color: 'red',
+        height: 'xs',
+        width: 'full',
+      },
+      title: 'function key and value',
+    },
+  ];
+
   testCases.forEach(({ args, expected, props, title }) => {
     test(title, () => {
       const result = processCssRule<ExampleProps>(
@@ -114,6 +114,104 @@ describe('processCssRule: regular props', () => {
   });
 });
 
-describe('processCssRule: responsive props', () => {
-  // type ResponsiveExampleProps = ResponsiveProps<ExampleProps>;
+describe('processCssRule: responsive props, no responsive usage', () => {
+  type ResponsiveExampleProps = ResponsiveProps<ExampleProps>;
+
+  const testCases: TestCase<ResponsiveExampleProps>[] = [
+    {
+      args: ['color'],
+      expected: '',
+      props: {
+        height: 'xs',
+        width: 'full',
+      },
+      title: 'missing prop',
+    },
+    {
+      args: ['color'],
+      expected: 'color: red;',
+      props: {
+        color: 'red',
+        height: 'xs',
+        width: 'full',
+      },
+      title: 'prop present',
+    },
+    {
+      args: ['height', 'height-key'],
+      expected: 'height-key: xs;',
+      props: {
+        color: 'red',
+        height: 'xs',
+        width: 'full',
+      },
+      title: 'string key',
+    },
+    {
+      args: ['height', undefined, 'tall'],
+      expected: 'height: tall;',
+      props: {
+        color: 'red',
+        height: 'xs',
+        width: 'full',
+      },
+      title: 'string value, no key',
+    },
+    {
+      args: ['height', 'height-key', 'tall'],
+      expected: 'height-key: tall;',
+      props: {
+        color: 'red',
+        height: 'xs',
+        width: 'full',
+      },
+      title: 'string key and value',
+    },
+    {
+      args: ['height', (props): string => props.width as string],
+      expected: 'full: xs;',
+      props: {
+        color: 'red',
+        height: 'xs',
+        width: 'full',
+      },
+      title: 'function key',
+    },
+    {
+      args: ['height', (props): string => props.width as string, 'purple'],
+      expected: 'full: purple;',
+      props: {
+        color: 'red',
+        height: 'xs',
+        width: 'full',
+      },
+      title: 'function key, string value',
+    },
+    {
+      args: [
+        'height',
+        (props): string => props.width as string,
+        (props): string => props.height + ' x2',
+      ],
+      expected: 'full: xs x2;',
+      props: {
+        color: 'red',
+        height: 'xs',
+        width: 'full',
+      },
+      title: 'function key and value',
+    },
+  ];
+
+  testCases.forEach(({ args, expected, props, title }) => {
+    test(title, () => {
+      const result = processCssRule<ResponsiveExampleProps>(
+        args[0],
+        args.length >= 1 ? args[1] : undefined,
+        args.length >= 2 ? args[2] : undefined
+      )(props);
+
+      expect(result).toEqual(expected);
+    });
+  });
 });
